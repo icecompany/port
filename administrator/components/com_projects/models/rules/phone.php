88399230324b9921c583db35cd06bcbd12e343d2
Anton <asharikov@icecompany.org>
2020-02-18 14:27:27 +0300

    Fix phone code

--- administrator/components/com_projects/models/rules/phone.php
+++ administrator/components/com_projects/models/rules/phone.php
@@ -4,7 +4,7 @@ defined('_JEXEC') or die;
 
 class JFormRulePhone extends FormRule
 {
-    protected $regex = '^\+\d{1,3}\s\(\d{3,5}\)\s\d{1,3}-\d{2}-\d{2}$';
+    protected $regex = '^\+\d{1,3}\s\(\d{2,5}\)\s\d{1,3}-\d{2}-\d{2}$';
 
     public function test(\SimpleXMLElement $element, $value, $group = null, \Joomla\Registry\Registry $input = null, \Joomla\CMS\Form\Form $form = null)
     {
